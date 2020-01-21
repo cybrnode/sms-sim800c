67825faa467d4060c1278e04576c8a18f8c7539f
Umar Ikram <ikramumar13@gmail.com>
2020-01-21 17:56:07 +0500

    v:0.0.2
    Added Comments

--- sendSms/sendSms.ino
+++ sendSms/sendSms.ino
@@ -1,8 +1,25 @@
+/******************************************************
+* sendSMS
+* 
+* This code was made public by CYBR node on January 21, 2020.
+* It is free to use and is inteded for educational purposes only.
+* CYBR node will not be responsible for any misuse of the code provided below.
+* 
+* Hardware used:
+*       -Arduino Uno
+*       -SIM800C 
+*
+* The Function:
+* sendSMS("Message", "Phone Number");
+*
+* You can also do this:
+* sendSMS("Hello from CYBR node!", "+921234567890");
+*******************************************************/
+
 #include <SoftwareSerial.h>
 
-SoftwareSerial gsmSerial(3, 2);
-String phone_no = "add phone number here"; //  "+921234567890"
-
+SoftwareSerial gsmSerial(3, 2); // Rx, Tx
+String phone_no = "add your phone number here"; // Example format "+921234567890"
 
 void setup(){
   Serial.begin(9600);
@@ -16,7 +33,7 @@ void loop(){
   updateSerial();
 }
 
-
+//SendSMS function
 void sendSMS(String sms, String ph){
   gsmSerial.println("AT");
   updateSerial();
@@ -34,7 +51,7 @@ void sendSMS(String sms, String ph){
   delay(60000);
 }
 
-
+//Sending command and Getting response from SIM800C
 void updateSerial(){
   if (gsmSerial.available()){
     Serial.write(Serial.read());
